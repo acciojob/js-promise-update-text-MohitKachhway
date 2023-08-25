20eaee7df6abe4871bcdf91afda57db6aeff3d35
Mohit kachhaway <130671896+MohitKachhway@users.noreply.github.com>
2023-08-25 08:16:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let prom = new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve("Hello, world!")
+	},1000)
+})
+let output = document.getElementById("output");
+prom.then((data)=>{
+	output.textContent = data;
+})
